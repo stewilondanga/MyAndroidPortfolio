9fc9a2c12ab8d6aae285fddef318105c02bf011f
Stewart ilondanga <stilondanga@gmail.com>
2019-08-17 13:06:43 +0300

    add app/src/main/java/stewilondanga/github/io/myandroidportfolio/WebViewActivity.java

--- app/src/main/java/stewilondanga/github/io/myandroidportfolio/WebViewActivity.java
+++ app/src/main/java/stewilondanga/github/io/myandroidportfolio/WebViewActivity.java
@@ -1,19 +1,19 @@
-package stewilondanga.github.io.myandroidportfolio;
-
-import android.app.Activity;
+package stewilondanga.github.io.myandroidportfolio;import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class WebViewActivity extends Activity {
 
-    private WebView webview;
+    private WebView webView;
 
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
         webView = (WebView) findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://stewilondanga.github.io");
+        webView.loadUrl("https://stewilondanga.github.io/");
+
     }
+
 }
