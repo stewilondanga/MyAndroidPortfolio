c026525c8b1316b333dbf72b122b93713214626d
Stewart ilondanga <stilondanga@gmail.com>
2019-08-17 12:23:30 +0300

    add app/src/main/java/stewilondanga/github/io/myandroidportfolio/MainActivity.java

--- app/src/main/java/stewilondanga/github/io/myandroidportfolio/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/myandroidportfolio/MainActivity.java
@@ -8,11 +8,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
 
     private Button button;
 
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         final Context context = this;
 
         super.onCreate(savedInstanceState);
@@ -20,8 +20,13 @@ public class MainActivity extends Activity{
 
         button = (Button) findViewById(R.id.buttonUrl);
 
-        button.setOnClickListener(new OnClickListener()){
+        button.setOnClickListener(new OnClickListener() {
 
-        }
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
