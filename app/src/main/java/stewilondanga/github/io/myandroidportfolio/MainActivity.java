73f51d00546383c4a6e0031382b6591c0664e0f0
Stewart ilondanga <stilondanga@gmail.com>
2019-08-17 12:13:56 +0300

    add app/src/main/java/stewilondanga/github/io/myandroidportfolio/MainActivity.java

--- app/src/main/java/stewilondanga/github/io/myandroidportfolio/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/myandroidportfolio/MainActivity.java
@@ -1,2 +1,10 @@
 package stewilondanga.github.io.myandroidportfolio;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
