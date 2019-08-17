f1a1dc071286e4c6fd4a3f64afc2b9a4c2562985
Stewart ilondanga <stilondanga@gmail.com>
2019-08-17 12:40:03 +0300

    add app/src/main/java/stewilondanga/github/io/myandroidportfolio/WebViewActivity.java

--- app/src/main/java/stewilondanga/github/io/myandroidportfolio/WebViewActivity.java
+++ app/src/main/java/stewilondanga/github/io/myandroidportfolio/WebViewActivity.java
@@ -3,3 +3,8 @@ package stewilondanga.github.io.myandroidportfolio;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+
+public class WebViewActivity extends Activity {
+
+
+}
